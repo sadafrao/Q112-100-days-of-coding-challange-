102dfc8f04c7a73b64a4c5b9cb882c4b5c930a6f
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 17:06:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// Q112
+// crete a  map that store name of countries and their capitl as 
+var countries = new Map();
+countries.set("Pakistan", "Islamabad");
+countries.set("India", "New Delhi");
+countries.set("China", "Beijing");
+countries.set("Japan", "Tokyo");
+console.log(countries);
---
+++ index.ts
@@ -0,0 +1,11 @@
+// Q112
+// crete a  map that store name of countries and their capitl as 
+const countries = new Map<string ,string>();
+countries.set("Pakistan","Islamabad");
+countries.set("India", "New Delhi");
+countries.set("China", "Beijing");
+countries.set("Japan", "Tokyo");
+console.log(countries);
+
+
+
